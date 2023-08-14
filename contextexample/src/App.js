8cd6421037865cd23e4aab235ee6f37e62be872c
Kutay Akpinar <kutayakpnar@gmail.com>
2023-08-14 12:40:55 +0300

    Context Basics

---
+++ contextexample/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {ThemeProvider} from './context/ThemeContext';
+import Container from './components/Container';
+
+
+function App() {
+  return (
+   <ThemeProvider>
+    <Container/>
+  </ThemeProvider>
+  );
+}
+
+export default App;
